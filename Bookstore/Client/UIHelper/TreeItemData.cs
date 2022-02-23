234ba99a452ee05278a595101d2e003e9c535124
Эмиль Воробьёв <vorobevemil@gmail.com>
2022-02-23 21:31:59 +0300

    Добавлена страница для создания книги

---
+++ Bookstore/Client/UIHelper/TreeItemData.cs
@@ -0,0 +1,16 @@
+﻿namespace Bookstore.Client.UIHelper
+{
+    public class TreeItemData
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        public HashSet<TreeItemData>? TreeItems { get; set; }
+
+        public TreeItemData(int id, string? title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
--- Bookstore/Shared/DbModels/BookModel.cs
+++ Bookstore/Shared/DbModels/BookModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,25 @@ namespace Bookstore.Shared.DbModels
 {
     public class BookModel : BaseEntity
     {
+        [Required(ErrorMessage = "Название книги обязательно для ввода")]
+        public override string? Title {get; set;}
+        [Required(ErrorMessage = "Напишите описание книги")]
         public string? AboutProduct { get; set; }
         public string? Annotation { get; set; }
-        public int AuthorId { get; set; }
+        [Required(ErrorMessage = "Выберите автора")]
+        public int? AuthorId { get; set; }
         public AuthorModel? Author { get; set; }
-        public int PublishingHouseId { get; set; }
+        [Required(ErrorMessage = "Выберите издание")]
+        public int? PublishingHouseId { get; set; }
         public PublishingHouseModel? PublishingHouse { get; set; }
-        public int CatalogId { get; set; }
+        [Required(ErrorMessage = "Выберите категорию")]
+        public int? CatalogId { get; set; }
         public CatalogModel? Catalog { get; set; }
-        public int NumberPages { get; set; }
-        public int YearPublication { get; set; }
-        public int Price { get; set; }
+        [Required(ErrorMessage = "Введите количество страниц")]
+        public int? NumberPages { get; set; }
+        [Required(ErrorMessage = "Введите год издания")]
+        public int? YearPublication { get; set; }
+        [Required(ErrorMessage = "Введите цену")]
+        public int? Price { get; set; }
     }
 }
